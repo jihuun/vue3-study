af85c537fa4c84efaf34d1145f43b1bb44a87bcf
Ji-Hun Kim <jihuun.k@gmail.com>
2025-10-11 21:16:24 +0900

    implement logout
    Signed-off-by: Ji-Hun Kim <jihuun.k@gmail.com>

--- src/view/UserProfile.vue
+++ src/view/UserProfile.vue
@@ -4,6 +4,7 @@ import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 const router = useRouter();
 const isLogin = ref(false);
+const userProfile = ref('');
 
 onMounted(async () => {
     const { data: {user} } = await supabase.auth.getUser();
@@ -12,6 +13,7 @@ onMounted(async () => {
         isLogin.value = true;
         console.log('User is logged in:');
         console.log(user.email);
+        userProfile.value = user;
     } else {
         isLogin.value = false;
         console.log('No user logged in');
@@ -19,13 +21,36 @@ onMounted(async () => {
         router.push('/');  /* 로그인 페이지로 이동 */
     }
 });
+
+const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut()
+    if (error) {
+        alert('Error logging out: ' + error.message);
+    } else {
+        alert('Logout successful!');
+        isLogin.value = false;
+        router.push('/');  /* 로그인 페이지로 이동 */
+    }
+}
 </script>
 
 <template>
     <div class="container" v-if="isLogin">
         <h1>User Profile</h1>
+        <p>{{ userProfile }}</p>
+        <button class="logout" @click="handleLogout">Logout</button>
     </div>
 </template>
 
 <style scoped lang="scss">
+  button {
+    background: transparent;
+    color: var(--main-color);
+    font-size: 16px;
+    margin-top: 40px;
+    &:hover{
+      opacity: 0.7;
+      text-decoration: underline;
+    }
+  }
 </style>
